3747e09f7e6ea022a79fa9f1c9ceeaa52400fbfe
alazar <alazarfikadu15@gmail.com>
2025-09-05 16:43:41 +0300

    update telebirrworker

--- handlers/telebirrWorker.js
+++ handlers/telebirrWorker.js
@@ -20,7 +20,7 @@ const opts = {
     capabilities: {
         alwaysMatch: {
             platformName: "Android",
-            "appium:deviceName": "10.0.0.4:41547",
+            "appium:deviceName": "10.0.0.4:41267 ",
             "appium:automationName": "UiAutomator2",
             "appium:appPackage": "cn.tydic.ethiopay",
             "appium:appActivity": "com.huawei.module_basic_ui.splash.LauncherActivity",
