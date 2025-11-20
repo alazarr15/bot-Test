d7c83b3866671a5963822ccd162b9abe765cc876
MikeWeDev <mikyasnegash2@gmail.com>
2025-11-20 14:26:55 +0300

    implmenting the regitration bonus amount and voulme of users 5

--- Model/BonusSettings.js
+++ Model/BonusSettings.js
@@ -25,7 +25,7 @@ const bonusSettingsSchema = new mongoose.Schema({
     registrationBonusLimit: {
         type: Number,
         required: true,
-        default: 1, // Default limit is 500 users
+        default: 2, // Default limit is 500 users
     },
     // ⭐ NEW: The current count of users who have received the registration bonus
     registrationBonusCount: {
